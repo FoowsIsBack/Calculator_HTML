dabe5df7e3d49a7c75cb468ed72d3ce6a2c10fdb
Foows - (Dave) <dbangcoyo11@gmail.com>
2025-03-30 09:29:18 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function number(val){
+    display.value += val;
+}
+
+function clear_number(){
+    display.value = "";
+}
+
+function calculate(){
+    display.value = eval(display.value) || "Imissyouuu :(";
+}
